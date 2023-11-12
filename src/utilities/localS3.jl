2a0e9c0b72419a9aca0ac2493106983a6732921a
Jay Sacco <jaysacco@sbcglobal.net>
2023-11-12 12:05:01 -0600

    Implement S3 list_objects Fix bug: stop duplicating header fields Fix bug: fix put and delete object, it wasn't working with multidirectory keys

--- src/utilities/localS3.jl
+++ src/utilities/localS3.jl
@@ -2,19 +2,6 @@ module localS3
 
 using AWS, HTTP, Dates
 
-version = v"1.1.0"
-status = 200
-
-"""
-To be added by _response function in this module
-"Date" => "TBD",
-"Content-Length" => "0",
-"""
-default_aws_response_headers = Pair[
-    "x-amz-id-2" => "x-amz-id-2",
-    "x-amz-request-id" => "No ID For local S3",
-    "Server" => "AmazonS3",
-]
 
 function create_error_xml(error_code::String, error_msg::String, resource)
     body = """
@@ -27,6 +14,34 @@ function create_error_xml(error_code::String, error_msg::String, resource)
     </Error>
     """
     return(body)
+end
+
+function create_list_bucket_result(bucket_name, keylist)
+    numkeys = length(keylist)
+    result_prefix = """
+    <ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+        <Name>$(bucket_name)</Name>
+        <Prefix/>
+        <KeyCount>$(numkeys)</KeyCount>
+        <MaxKeys>1000</MaxKeys>
+        <IsTruncated>$(numkeys > 1000)</IsTruncated>
+        """
+    result_contents = ""
+    for k in keylist
+        result_contents *= """
+        <Contents>
+            <Key>$(k["Key"])</Key>
+            <LastModified>TBD</LastModified>
+            <ETag>TBD</ETag>
+            <Size>TBD</Size>
+            <StorageClass>STANDARD</StorageClass>
+        </Contents>
+        """
+    end
+    result_end = """
+        </ListBucketResult>
+        """
+    return(result_prefix * result_contents * result_end)
 end
 
 """
@@ -56,37 +71,49 @@ end
 
 # Override AWS S3 requests to use the local file system instead of S3
 function request(creds::AWS.AWSCredentials, request::AWS.Request)
-	# NOTE: we could use the URIs.jl package to parse but that seems overweight for what we need to do here
-    # request.resource e.g. /my.bucket?CreateBucketConfiguration=%3CCreateBucketConfiguration%20xmlns%3D%22http%3A%2F%2Fs3.amazonaws.com%2Fdoc%2F2006-03-01%2F%22%3E%0A%20%20%20%20%3CLocationConstraint%3Eus-east-2%3C%2FLocationConstraint%3E%0A%3C%2FCreateBucketConfiguration%3E%0A"
-    # Convert encoded characters in the URL with the actual character
-    resource = replace(request.resource, "%3A" => ":")
-    resource = replace(resource, "%5C" => "/")
-    resource = lstrip(resource, ['/']) # Remove leading forward slash
-    if occursin("?", resource)
-        resource = split(resource,"?") # Separate any query parameters from the requested resource
-        query_parms = resource[2] # e.g. "CreateBucketConfiguration=%3CCreateBucketConfiguration%20xmlns%3D%22http%3A%2F%2Fs3.amazonaws.com%2Fdoc%2F2006-03-01%2F%22%3E%0A%20%20%20%20%3CLocationConstraint%3Eus-east-2%3C%2FLocationConstraint%3E%0A%3C%2FCreateBucketConfiguration%3E%0A"
-        resource = resource[1]
-    else
-        query_parms = ""
-    end
 
+    # Set default values
+    version = v"1.1.0"
+    status = 200
+    
+    """
+    Must be added during request processing in this module
+    "Date" => "TBD",
+    "Content-Length" => "0",
+    "Content-Type" =? TBD,
+    """
+    response_headers = Pair[
+        "x-amz-id-2" => "x-amz-id-2",
+        "x-amz-request-id" => "No ID For local S3",
+        "Server" => "AmazonS3",
+    ]
+    
+    # request.resource e.g. /my.bucket?CreateBucketConfiguration=%3CCreateBucketConfiguration%20xmlns%3D%22http%3A%2F%2Fs3.amazonaws.com%2Fdoc%2F2006-03-01%2F%22%3E%0A%20%20%20%20%3CLocationConstraint%3Eus-east-2%3C%2FLocationConstraint%3E%0A%3C%2FCreateBucketConfiguration%3E%0A"
+    resource_url = HTTP.URIs.URI(request.url)
+    resource_path = resource_url.path
+    # Convert encoded characters in the URL with the actual character
+    resource = HTTP.URIs.unescapeuri(resource_path)
+    resource = lstrip(resource, ['/']) # Remove leading forward slash
+    query_parms = HTTP.URIs.queryparams(resource_url)
     method = request.request_method # PUT, GET etc.
     user = creds.user_arn
     status = 200
     error_code = ""
     error_msg = ""
-    response_headers = default_aws_response_headers
     body = ""
     content_type = "text/html" # If non specified, treat as text
     # S3 resources come in the form of <bucket>/s3://<fully qualified path to data key> e.g. 
     # "TestBucket/s3://C:/Users/jaysa/Documents/git/cfi/CFI_Storage/jaysacco/TestBucket/datakey1"
     s3_resource = split(resource, "s3://")
-
+    @show(request)
+    @show(method)
+    @show(s3_resource)
+    @show(query_parms)
     if method == "PUT"
         #
         # Create Bucket
         #
-        if startswith(query_parms, "CreateBucketConfiguration")
+        if haskey(query_parms, "CreateBucketConfiguration")
             # Create bucket https://docs.aws.amazon.com/AmazonS3/latest/API/API_CreateBucket.html
             # resource is the name of the directory to create, assume it starts with a / character
             # Make sure the bucket doesn't already exist, bucket names must be globally unique even between user accounts
@@ -146,7 +173,9 @@ function request(creds::AWS.AWSCredentials, request::AWS.Request)
                 if length(split_md) > 1
                     append!(metadata, [split_md[2] => md[2]])
                 end
-            end  
+            end
+            # Make sure all intermediate folders specified by the object key exist
+            mkpath(dirname(fq_data_key))
             open(fq_data_key, "w") do file
                 write(file, request.content)
             end
@@ -183,7 +212,39 @@ function request(creds::AWS.AWSCredentials, request::AWS.Request)
             # Add metadata to response headers
             append!(response_headers, metadata)
             body = object_data
-        end # get object
+        # end get object
+        #
+        # List objects. Returns a list of keys (up to 1000) of the objects in the specified bucket
+        #
+        elseif haskey(query_parms, "list-type")
+            # The fully qualified path to the bucket is the first item in the s3 resource array
+            fq_bucket = s3_resource[1]
+            # Get the fully qualified path up to the bucket i.e. the path prefix to the bucket. This is
+            # the path up to and including the user name, which is the last element before the bucket name starts.
+            # Note that the bucket name could be a path to a subdirectory so we can't just remove the last element of the path
+            path_prefix = splitpath(fq_bucket)
+            # Remove the last path elements up to and including the user name
+            while pop!(path_prefix ) != user end
+            # Add the user name back in with trailing slash to complete the prefix. Use normpath
+            # to make sure the trailing slash is converted to the local OS path separator
+            # This will make the returned list of object keys start with the bucket name, not a path separator
+            path_prefix = normpath(joinpath(push!(path_prefix, user * "/")))
+            bucket_name = replace(fq_bucket, path_prefix => "")
+            object_list = []
+            for k in walkdir(fq_bucket)
+                # If there are files in this iteration
+                for f in k[3]
+                    # Don't include metadata in the key list
+                    if !endswith(f, ".metadata")
+                        # Form the key to the file by joining the fully qualified directory path to the file name
+                        fkey = replace(joinpath(k[1], f), path_prefix => "")
+                        push!(object_list, Dict("Key" => fkey))
+                    end
+                end
+            end
+            push!(response_headers, "Content-Type" => "application/xml")
+            body = replace(create_list_bucket_result(bucket_name, object_list), "\n" =>"")
+        end # list objects
     elseif method == "HEAD"
         #
         # get_meta data
@@ -200,14 +261,26 @@ function request(creds::AWS.AWSCredentials, request::AWS.Request)
         end # get_meta data
     elseif method == "DELETE"
         #
-        # delete object.  If the object is a directory, all directory contents are deleted as well.
+        # delete object.  If the object is a multidirectory key, each directory in the key will
+        # be deleted if it contains no files after the key is deleted.
         # if the object doesn't exist, doesn't return error
         #
         if length(s3_resource) > 1
+            bucket_name = chop(s3_resource[1])
             fq_data_key = last(s3_resource)
             fq_metadata_key = fq_data_key * ".metadata"
             rm(fq_data_key, force=true, recursive=true)
             rm(fq_metadata_key, force=true, recursive=true)
+            # Process multidirectory key
+            fq_data_key = splitdir(fq_data_key)[1] # Remove the last element of the key path
+            # While we haven't gotten back to the bucket the key resides in
+            while(!endswith(fq_data_key, bucket_name))
+                # if the last directory in the updated key path is empty, remove it
+                if length(readdir(fq_data_key)) == 0
+                    rm(fq_data_key)
+                end
+                fq_data_key = splitdir(fq_data_key)[1] # Remove the last element of the key path
+            end
             status = 204
             # End delete object
         #
@@ -240,7 +313,7 @@ function request(creds::AWS.AWSCredentials, request::AWS.Request)
         end # delete object
     end # method == xx
     println("Sending $status response")
-    return _response(status = status, headers = response_headers, body = body)
+    return _response(version = version, status = status, headers = response_headers, body = body)
 end
 
 """
@@ -257,7 +330,7 @@ Returns an HTTP.Messages.Response
 function _response(;
     version::VersionNumber=version,
     status::Int64=status,
-    headers::Array=default_aws_response_headers,
+    headers::Array,
     body::String=body,
 )
     response = HTTP.Messages.Response()
